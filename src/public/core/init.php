9f2df0c239333c85d4e0d8d2a2449db1924fc7b3
ernst <ernst@ernsts-MacBook-Pro.local>
2014-12-10 21:59:57 +0100

    cleanup

--- src/public/core/init.php
+++ src/public/core/init.php
@@ -6,10 +6,9 @@
 	require 'functions/general.php';
 	require 'functions/users.php';
 
-	$user_data = array();
+	$user_data 	= array();
+	$errors 	= array();
 
 	if ( logged_in() )
 		$user_data = user_data($_SESSION['username'], 'username','level');
-
-	$errors = array();
 ?>
